7aa6bb9c1c1f9a01c3bd66eb4aab0104945c81ee
LibLev <libardi.levente@gmail.com>
2021-01-16 08:38:07 +0100

    add files to repository

---
+++ src/main/java/levente/libardi/abris/AbrisApplication.java
@@ -0,0 +1,13 @@
+package levente.libardi.abris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbrisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/levente/libardi/abris/AbrisApplicationTests.java
@@ -0,0 +1,13 @@
+package levente.libardi.abris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
